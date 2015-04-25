73c6e8b5e934beb1d3186bd62800f10427e55888
Swaraj Banerjee <swarajban@gmail.com>
2015-04-25 11:23:41 -0700

    added base tornado application

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+from tornado.ioloop import IOLoop
+from tornado.web import RequestHandler, Application, url
+
+
+
+class HelloHandler(RequestHandler):
+	def get(self):
+		print("get")
+		self.write("Hello World")
+
+	def post(self):
+		print("post")
+		self.write("MM response...")
+
+
+def make_app():
+	return Application(
+		[
+		url(r"/", HelloHandler)
+		]
+	)
+
+
 def main():
-	print("hello")
+	app = make_app()
+	app.listen(8000)
+	IOLoop.current().start()
 
 
 if __name__ == "__main__":
